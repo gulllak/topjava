cce37ab9930fe2aff718c63bd337c97e09fd75db
”Evgenii <gulllak@yandex.ru>
2022-06-23 10:18:00 +0300

    add_meals_db

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -10,3 +11,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2022-07-05', 'breakfast', 1000, 100000),
+       ('2022-07-06', 'dinner', 1000, 100000),
+       ('2022-07-05','breakfast', 500, 100001),
+       ('2022-07-06','dinner', 1500, 100001);
